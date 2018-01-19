9889a6193748735d2eaf5e59c0d28ed8fd901c3e
MrAaronCasanova <1boris231@gmail.com>
2018-01-18 17:06:10 -0800

    finished mini authDemo project, about to apply these concepts to the yelpCamp project

--- app.js
+++ app.js
@@ -14,6 +14,7 @@ mongoose.Promise = global.Promise;
 
 app.set('view engine', 'ejs');
 
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(expressSession({
   secret: 'Moose is the best and cutest dog in the world!',
   resave: false,
@@ -22,17 +23,72 @@ app.use(expressSession({
 app.use(passport.initialize());
 app.use(passport.session());
 
+passport.use(new LocalStrategy(User.authenticate()));
 passport.serializeUser(User.serializeUser());
 passport.deserializeUser(User.deserializeUser());
+
+// ================================================
+// BASIC ROUTES
+// ================================================
 
 app.get('/', function (req, res) {
   res.render('home');
 });
 
-app.get('/secret', function (req, res) {
+app.get('/secret', isLoggedIn, function (req, res) {
   res.render('secret');
 });
 
+// ================================================
+// AUTH ROUTES
+// ================================================
+
+app.get('/register', function (req, res) {
+  res.render('register');
+});
+
+app.post('/register', function (req, res) {
+  User.register(new User({ username: req.body.username }), req.body.password, function (err, user) {
+    if (err) {
+      console.log(err);
+      return res.render('register');
+    }
+
+    passport.authenticate('local')(req, res, function () {
+      res.redirect('/secret');
+    });
+  });
+});
+
+// ================================================
+// LOGIN ROUTES
+// ================================================
+
+app.get('/login', function (req, res) {
+  res.render('login');
+});
+
+app.post('/login', passport.authenticate('local', {
+  successRedirect: '/secret',
+  failureRedirect: '/login',
+}), function (req, res) {});
+
+app.get('/logout', function (req, res) {
+  req.logout();
+  res.redirect('/');
+});
+
+function isLoggedIn(req, res, next) {
+  if (req.isAuthenticated()) {
+    return next();
+  }
+
+  res.redirect('/login');
+}
+
+// ================================================
+// Server Config
+// ================================================
 app.listen(3000, function () {
   console.log('Server started on port 3000');
 });
